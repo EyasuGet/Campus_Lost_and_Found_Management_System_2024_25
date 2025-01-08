678ca913b28337699a19cbc7a9707042c2821495
eyasu <eyasugetaneh0@gmail.com>
2025-01-08 20:51:03 +0300

    modified main

--- lost-and-found_backend/src/main.ts
+++ lost-and-found_backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express'
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use("/uploads", express.static(join(".","uploads")))
+  app.enableCors({
+    origin: 'http://127.0.0.1:5501',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true, // Throw an error if unknown properties are sent
+    transform: true,
+  }))
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
